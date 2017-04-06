c68f5e0d091e211ca95457dd591beacebf0bda0a
frsmtv <fsimitchiev@gmail.com>
2017-04-07 01:42:38 +0200

    oo

--- js/main.js
+++ js/main.js
@@ -1,19 +1,17 @@
 $(document).ready(function() {
 
-    // **** PROJECTS -> ACCORDION ****
-    // $('.accordion').accordion({
-    //     "transitionSpeed": 400
-    // });
-
     // **** SCROLLA ****
     $('.animate').scrolla();
 
-    $('li').mouseover(function(){
-      $(this).find('h2').css('visibility', 'visible');
+    // **** PROJECTS PAGE ANIMATION HOVER ****
+    $('#portfolio li').mouseover(function(){
+        $('#portfolio li').not(this).css('opacity', '.5');
+        $(this).css('transform', 'scale(1.05)');
     });
 
-    $('li').mouseout(function(){
-      $(this).find('h2').css('visibility', 'hidden');
+    $('#portfolio li').mouseout(function(){
+        $('#portfolio li').css('opacity', '1');
+        $(this).css('transform', 'scale(1)');
     });
 
 });
