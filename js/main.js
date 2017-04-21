0bf17dac9043942a5ca335e22f04a85eb03de9df
frsmtv <fsimitchiev@gmail.com>
2017-04-21 13:05:02 +0200

    eeeee

--- js/main.js
+++ js/main.js
@@ -23,7 +23,50 @@ $(document).ready(function() {
         }, 1800);
    });
 
+   //  NAV HOVER EFFECT ON SECTIONS
+
+   // MAIN MENU
+   $('#main-nav a').not('.active').mouseover(function(){
+     $('#intro').css('opacity', '.4');
+     $('section').css('opacity', '.4');
+     $('section').css('-webkit-filter', 'grayscale(100%)');
+     $('#main-nav a').not(this).css('opacity', '.3');
+   })
+
+   $('#main-nav a').not('.active').mouseout(function(){
+     $('#intro').css('opacity', '');
+     $('section').css('opacity', '');
+     $('section').css('-webkit-filter', 'grayscale(0%)');
+     $('#main-nav a').not(this).css('opacity', '');
+   })
+
+   // SINGLE NAV
+   $('.next').mouseover(function(){
+     $('section').css('opacity', '.4');
+     $('section').css('-webkit-filter', 'grayscale(100%)');
+     $('#main-nav a').not(this).css('opacity', '.3');
+   })
+
+   $('.next').mouseout(function(){
+     $('section').css('opacity', '');
+     $('section').css('-webkit-filter', 'grayscale(0%)');
+     $('#main-nav a').not(this).css('opacity', '');
+   })
+
+   $('.prev').mouseover(function(){
+     $('section').css('opacity', '.4');
+     $('section').css('-webkit-filter', 'grayscale(100%)');
+     $('#main-nav a').not(this).css('opacity', '.3');
+   })
+
+   $('.prev').mouseout(function(){
+     $('section').css('opacity', '');
+     $('section').css('-webkit-filter', 'grayscale(0%)');
+     $('#main-nav a').not(this).css('opacity', '');
+   })
+
   //  MODAL
+  // (Always put this function on bottom of document)
    $('#modal-content-1').apFullscreenModal({
      openSelector: '#formlink',
      closeSelector: '.close-modal, #send-1'
