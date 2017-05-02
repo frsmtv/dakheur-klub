ef4b0239cd60ecd97a7cc222de540d46d9f98fdc
frsmtv <fsimitchiev@gmail.com>
2017-05-02 16:46:09 +0200

    yyy

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@ $(document).ready(function() {
     // PROJECTS PAGE ANIMATION HOVER
     $('#portfolio li').mouseover(function(){
         $('#portfolio li').not(this).css('opacity', '.5');
-        $('#portfolio li').not(this).css('transform', 'scale(.95)');
-        $(this).css('transform', 'scale(1.01)');
+        // $('#portfolio li').not(this).css('transform', 'scale(.95)');
+        // $(this).css('transform', 'scale(1.01)');
     });
 
     $('#portfolio li').mouseout(function(){
         $('#portfolio li').css('opacity', '1');
-        $(this).css('transform', 'scale(1)');
-        $('#portfolio li').not(this).css('transform', 'scale(1)');
+        // $(this).css('transform', 'scale(1)');
+        // $('#portfolio li').not(this).css('transform', 'scale(1)');
     });
 
     // CONTACT FORM ANIMATION
