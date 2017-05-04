ae5c2dcad1ee85bbd03a36a62836740266e720a8
frsmtv <fsimitchiev@gmail.com>
2017-05-04 14:13:16 +0200

    uuuu

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
 
-    // PROJECTS PAGE ANIMATION HOVER
+    // PORTFOLIO PAGE ANIMATION HOVER
     $('#portfolio li').mouseover(function(){
-        $('#portfolio li').not(this).css('opacity', '.5');
+        $('#portfolio li img').css('opacity', '.8');
+        $('div.status').show().addClass('animated flipInX');
     });
 
     $('#portfolio li').mouseout(function(){
-        $('#portfolio li').css('opacity', '1');
+        $('#portfolio li img').css('opacity', '1');
+        $('div.status').hide();
     });
+
+    // PORTFOLIO CAPTIONS
 
     // CONTACT FORM ANIMATION
     $('#formlink').click(function(){
@@ -22,14 +26,14 @@ $(document).ready(function() {
    //  NAV HOVER EFFECT ON SECTIONS
    // MAIN MENU
    $('#main-nav a').not('.active').mouseover(function(){
-     $('#intro').css('opacity', '.4');
-     $('section').css('opacity', '.4');
+    //  $('#intro').css('opacity', '.4');
+    //  $('section').css('opacity', '.4');
      $('#main-nav a').not(this).css('opacity', '.3');
    })
 
    $('#main-nav a').not('.active').mouseout(function(){
-     $('#intro').css('opacity', '');
-     $('section').css('opacity', '');
+    //  $('#intro').css('opacity', '');
+    //  $('section').css('opacity', '');
      $('#main-nav a').not(this).css('opacity', '');
    })
 
